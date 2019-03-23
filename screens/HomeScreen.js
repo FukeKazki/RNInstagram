4eb954c8c0ba5d8b921d21fc0e757c2c16f525ff
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-03-23 16:32:58 +0900

    Add cards in HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,7 +15,7 @@ import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
-import { Header } from 'react-native-elements';
+import { Header, Card, Icon } from 'react-native-elements';
 
 import Carousel from 'react-native-snap-carousel';
 
@@ -90,94 +90,71 @@ export default class HomeScreen extends React.Component {
             enableSnap={false}
             firstItem={1}
           />
-          {/* <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View> */}
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+          
+          <Card
+            title='usename'
+            image={require('../assets/images/50_50.png')}
+          >
+            <View style={styles.iconsContainer}>
+              <Icon
+                name='star-border'
+              />
+              <Icon
+                name='textsms'
+                iconStyle={{marginLeft: 10}}
+              />
+              <Icon
+                name='near-me'
+                iconStyle={{marginLeft: 10}}
+              />
+              <Icon
+                name='bookmark-border'
+                iconStyle={{marginLeft: 200}}
+              />
             </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
+            <Text style={styles.cardText}>
+            {`
+            #シナモンコーデ ／(o・ω・o)＼💭
+            シナモン意識で全身ホワイト♡
+            ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+            フリフリ〜✨リボンなしでも可愛い🎀
+            これも #milkcocoa のワンピです💍
+            ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+            レースくつした #loveran 💅🏻
+            シューズ #niceclaup 💕
+            ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+            #サンリオピューロランド #シナモン #みりにゃ服
+            `}
             </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
+          </Card>
+          <Card
+            title='usename'
+            image={require('../assets/images/50_50.png')}
+          >
+            <Text>textarea</Text>
+          </Card>
+          <Card
+            title='usename'
+            image={require('../assets/images/50_50.png')}
+          >
+            <Text>textarea</Text>
+          </Card>
+          <Card
+            title='usename'
+            image={require('../assets/images/50_50.png')}
+          >
+            <Text>textarea</Text>
+          </Card>
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
   },
   contentContainer: {
     paddingTop: 10,
@@ -189,76 +166,10 @@ const styles = StyleSheet.create({
     flex: 3,
     borderRadius: 40,
   },
-  welcomeContainer: {
-    alignItems: 'center',
+  cardText: {
     marginTop: 10,
-    marginBottom: 20,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  iconsContainer: {
+    flexDirection: 'row',
   },
 });
