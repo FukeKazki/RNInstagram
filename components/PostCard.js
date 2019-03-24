c62402e82e309a032ab830bad7803a6114a9a133
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-03-24 14:49:49 +0900

    Add carousel & pagination

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
-import { Card, Icon, Avatar } from 'react-native-elements';
-import Carousel from 'react-native-snap-carousel';
+import { Card, Icon, Avatar, Image } from 'react-native-elements';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import Layout from '../constants/Layout';
+
+const Images = [{ uri: 'https://scontent-nrt1-1.cdninstagram.com/vp/a78056e25739feafa0a0774eda38848c/5D17F577/t51.2885-15/e35/54800420_2369011479987240_7022525315400684312_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com' }];
 
 export default class PostCard extends React.Component {
-    _renderTitleComponent () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSlide: 0,
+      carouselItems: [
+        {
+          image: Images[0],
+        },
+        {
+          image: Images[0],
+        },
+        {
+          image: Images[0],
+        },
+      ],
+    }
+  }
+  _renderItem ({item, index}) {
+    return(
+      <View style={styles.carouselItems}>
+        <Image
+          source={item.image}
+          style={{height: 250}}
+        />
+      </View>
+    );
+  }
+    _renderTitleComponent() {
         return(
           <View style={styles._renderTitleComponent}>
             <Avatar
@@ -15,7 +45,7 @@ export default class PostCard extends React.Component {
             <Text style={styles.cardTitle}>otani_emiri</Text>
             <Icon
               name='more-horiz'
-              iconStyle={{marginLeft: 190}}
+              iconStyle={{marginLeft: 180}}
             />
           </View>
         );
@@ -24,9 +54,17 @@ export default class PostCard extends React.Component {
         return(
             <Card
                 title={this._renderTitleComponent()}
-                image={{uri:'https://scontent-nrt1-1.cdninstagram.com/vp/a78056e25739feafa0a0774eda38848c/5D17F577/t51.2885-15/e35/54800420_2369011479987240_7022525315400684312_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com'}}
-                imageStyle={{height: 300}}
             >
+                <Carousel
+                  style={styles.carouselStyle}
+                  ref={c => {this._carousel = c}}
+                  data={this.state.carouselItems}
+                  renderItem={this._renderItem}
+                  sliderWidth={300}
+                  itemWidth={200}
+                  firstItem={0}
+                  onSnapToItem={index=>this.setState({activeSlide: index})}
+                />
                 <View style={styles.iconsContainer}>
                     <Icon
                     name='favorite-border'
@@ -39,9 +77,21 @@ export default class PostCard extends React.Component {
                         name='send'
                         iconStyle={{marginLeft: 10}}
                     />
+                    <Pagination
+                      dotsLength={this.state.carouselItems.length}
+                      activeDotIndex={this.state.activeSlide}
+                      dotStyle={{
+                        width: 5,
+                        height: 5,
+                        borderRadius: 2.5,
+                        backgroundColor: 'rgba(225,225,225,0.92)'
+                      }}
+                      inactiveDotOpacity={0.4}
+                      inactiveDotScale={0.6}
+                    />
                     <Icon
                         name='bookmark-border'
-                        iconStyle={{marginLeft: 200}}
+                        iconStyle={{marginLeft: 70}}
                     />
                 </View>
                 <Text style={styles.cardText}>
@@ -69,6 +119,7 @@ const styles = StyleSheet.create({
     },
     iconsContainer: {
       flexDirection: 'row',
+      marginTop: 10,
     },
     _renderTitleComponent: {
       flexDirection: 'row',
@@ -79,4 +130,10 @@ const styles = StyleSheet.create({
     cardTitle: {
       marginLeft: 10,
     },
+    // carouselStyle: {
+    //   flex:  1,
+    // },
+    // carouselItems: {
+    //   flex: 1,
+    // },
   });
